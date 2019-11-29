b63ca33cc163ae420bd640e6ef1a23402bbf2ead
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-11-29 13:11:25 +0000

    build.sh: remove ls commands

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,1 @@
-ls ${PREFIX}
-ls ${PREFIX}/include
-
-
 $PYTHON -m pip install . --no-deps --ignore-installed -vv --global-option "build" --global-option "--hdf5=${PREFIX}" --global-option "--native=False" --global-option "--sse2=True" --global-option "--avx2=False" --global-option "--openmp=False" --global-option "--cpp11=False"
