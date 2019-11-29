75936a02fc92f46e9d78bb18737ffc3a255dd566
Thomas VINCENT <thomas.vincent@esrf.fr>
2019-11-29 13:28:05 +0100

    Add build options to disable c++11, native build, openmp and avx2

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,4 +2,4 @@ ls ${PREFIX}
 ls ${PREFIX}/include
 
 
-$PYTHON -m pip install . --no-deps --ignore-installed -vv --global-option "build" --global-option "--hdf5=${PREFIX}"
+$PYTHON -m pip install . --no-deps --ignore-installed -vv --global-option "build" --global-option "--hdf5=${PREFIX}" --global-option "--native=False" --global-option "--sse2=True" --global-option "--avx2=False" --global-option "--openmp=False" --global-option "--cpp11=False"
