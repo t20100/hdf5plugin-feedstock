2b079dd245671acece19b290fae2d25c011213e1
Thomas VINCENT <thomas.vincent@esrf.fr>
2019-12-16 11:27:12 +0100

    Build with C++11 on macOS and Linux (snappy library)

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,1 +1,1 @@
-$PYTHON -m pip install . --no-deps --ignore-installed -vv --global-option "build" --global-option "--hdf5=${PREFIX}" --global-option "--native=False" --global-option "--sse2=True" --global-option "--avx2=False" --global-option "--openmp=False" --global-option "--cpp11=False"
+$PYTHON -m pip install . --no-deps --ignore-installed -vv --global-option "build" --global-option "--hdf5=${PREFIX}" --global-option "--native=False" --global-option "--sse2=True" --global-option "--avx2=False" --global-option "--openmp=False"
