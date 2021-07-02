cbfa737e3389275e50581989c213b33b5e909547
Thomas VINCENT <thomas.vincent@esrf.fr>
2021-07-02 17:20:59 +0200

    use env.var. to set build options

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,1 +1,3 @@
-%PYTHON% -m pip install . --no-deps --ignore-installed -vv --global-option "build" --global-option --hdf5="%LIBRARY_PREFIX%" --global-option --native=False --global-option --sse2=True --global-option --avx2=False --global-option --openmp=False
+set HDF5PLUGIN_HDF5_DIR=%LIBRARY_PREFIX%
+
+%PYTHON% -m pip install . --no-deps --ignore-installed -vv
