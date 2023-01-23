8eb077431744e103b8c8463732ce773a2bf368ef
Thomas VINCENT <thomas.vincent@esrf.fr>
2023-01-23 14:30:07 +0100

    Use embedded HDF5 headers on Windows to build SZ3 with c++14

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,6 @@
-set HDF5PLUGIN_HDF5_DIR=%LIBRARY_PREFIX%
+REM HDF5 header files require the following patch to be used with C++14:
+REM https://github.com/HDFGroup/hdf5/pull/2269
+REM Use headers embedded in hdf5plugin until HDF5 with this patch is available.
+REM set HDF5PLUGIN_HDF5_DIR=%LIBRARY_PREFIX%
 
 %PYTHON% -m pip install . --no-deps --ignore-installed -vv
